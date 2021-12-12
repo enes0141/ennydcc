5cb5e5c2107ae600c5cc6f49260152fc131c9e93
enes0141 <33023383+enes0141@users.noreply.github.com>
2021-12-12 16:10:04 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,6 @@
+echo off
+color 0f
+cls
+:a
+node enny.js
+goto a
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://www.hizliresim.com/4fDeDY Selamlar, **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle  :wink:');
+
+};
